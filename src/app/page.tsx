44bd15efd610ba608ba21ff20f524a1fb2cc1c41
TashaMia <rabbit-t03@yandex.ru>
2023-09-04 23:38:09 +0600

    fix: fixed redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,6 @@
-'use client'
-import { useRouter } from 'next/navigation'
 import ScheduleSection from './[month]/page'
-import { useEffect } from 'react'
 
 export default function Home() {
-    const navigate = useRouter()
-    useEffect(() => {
-        navigate.push('/month')
-    }, [])
     return (
         <main className="flex min-h-screen min-w-screen flex-col  justify-center items-center">
             <ScheduleSection />
